d8322f443110a25c6f4a326069de032c41f5b1cd
slime830 <ossar830@gmail.com>
2023-03-06 21:57:52 +0900

    20230306 add p_6 README

---
+++ p_6/CommandLineExample.java
@@ -0,0 +1,12 @@
+package p_6;
+
+public class CommandLineExample {
+
+    public static void main(String[] args){
+        if(args[0].equals("1")){
+            System.out.println("ほげほげほげほげ");
+        }else{
+            System.out.println("ふがふがふがふが");
+        }
+    }
+}
---
+++ p_6/README.md
@@ -0,0 +1,7 @@
+# 6章 コマンドライン引数
+
+- とりあえずのサンプルコード
+[CommandLineExample](./CommandLineExample.java)
+
+- 実際の使い方の例（ファイル名の指定）
+[FileContentPrinter](./FileContentPrinter.java)
